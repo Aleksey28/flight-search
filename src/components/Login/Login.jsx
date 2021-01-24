0607154f94dbec3bc64f25db6e0ad970922b5a0f
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-01-24 16:46:07 +0400

    feat: added styles for login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import classes from "./Login.module.css";
 
 const Login = () => {
   return (
-    <div>
-      <form action="">
-        <h1>Simple Flight Check</h1>
-        <input type="text" name="" id=""/>
-        <input type="password" name="" id=""/>
-        <button type="submit">Войти</button>
+    <div className={classes.login}>
+      <form action="" className={classes.form}>
+        <h1 className={classes.title}>Simple Flight Check</h1>
+        <label className={classes.label}>Логин:</label>
+        <input className={classes.input} type="text" name="" id=""/>
+        <label className={classes.label}>Пароль:</label>
+        <input className={classes.input} type="password" name="" id=""/>
+        <button className={classes.button} type="submit">Войти</button>
       </form>
     </div>
   );
