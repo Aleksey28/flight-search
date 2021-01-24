ad98cb83c31eab6b1a09863112736b9727fed2bb
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-01-24 15:51:03 +0400

    feat: added component login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <form action="">
+        <h1>Simple Flight Check</h1>
+        <input type="text" name="" id=""/>
+        <input type="password" name="" id=""/>
+        <button type="submit">Войти</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
