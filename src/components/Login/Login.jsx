1398b85b7e2c7df95c0ce9293ad528a12ba1cb2f
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-01-24 19:45:53 +0400

    feat: added validation for login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,68 @@
 import React from "react";
 import classes from "./Login.module.css";
+import cn from "classnames";
+import { useFormik } from "formik";
+
+const initialValues = {
+  email: "",
+  password: "",
+};
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.password) {
+    errors.password = "Поле обязательно для заполнения.";
+  }
+
+  if (!values.email) {
+    errors.email = "Поле обязательно для заполнения.";
+  } else if (
+    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+  ) {
+    errors.email = "Некорректный e-mail.";
+  }
+  return errors;
+};
 
 const Login = () => {
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validate,
+  });
+
   return (
     <div className={classes.login}>
-      <form action="" className={classes.form}>
+
+      <form onSubmit={formik.handleSubmit} className={classes.form}>
         <h1 className={classes.title}>Simple Flight Check</h1>
-        <label className={classes.label}>Логин:</label>
-        <input className={classes.input} type="text" name="" id=""/>
-        <label className={classes.label}>Пароль:</label>
-        <input className={classes.input} type="password" name="" id=""/>
-        <button className={classes.button} type="submit">Войти</button>
+        <label htmlFor="email" className={classes.label}> Логин: </label>
+        <input id="email"
+               name="email"
+               type="email"
+               onChange={formik.handleChange}
+               value={formik.values.email}
+               className={cn(classes.input, { [classes.input_error]: formik.errors.email })}/>
+        {formik.errors.email
+         ? <span className={classes.error}>{formik.errors.email}</span>
+         : null}
+        <label htmlFor="password" className={classes.label}> Пароль: </label>
+        <input id="password"
+               name="password"
+               type="password"
+               onChange={formik.handleChange}
+               value={formik.values.password}
+               className={cn(classes.input, { [classes.input_error]: formik.errors.password })}/>
+        {formik.errors.password
+         ? <span className={classes.error}>{formik.errors.password}</span>
+         : null}
+        <button type="submit" className={classes.button}>Войти</button>
       </form>
     </div>
   );
