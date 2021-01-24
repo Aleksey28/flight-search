ce64a94bd0c02ec8b892daa782b8a6c3a938a186
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-01-24 15:55:52 +0400

    feat: added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import React from "react";
+import { Route, Switch } from "react-router";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div className="page">
-      Login
+      <Switch>
+        <Route path={'/sign-in'}>
+          <Login />
+        </Route>
+        <Route path={'/'}>
+          base url
+        </Route>
+      </Switch>
     </div>
   );
 }
