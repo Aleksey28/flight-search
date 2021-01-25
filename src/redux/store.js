6e3b02f643ef4ae17fd5c43eb17f45f4de409435
Aleksey Popow <alekseypopow1995@yandex.ru>
2021-01-26 01:21:55 +0400

    feat: added saga and loading flifghts

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,21 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import loginReducer from "./loginReducer";
 import searchReducer from "./searchReducer";
+import createSagaMiddleware from "redux-saga";
+import watchFetchQuotes from "./sagas";
 
 const reducers = combineReducers({
   pageLogin: loginReducer,
   pageSearchFlights: searchReducer,
 });
 
-const store = createStore(reducers);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  reducers,
+  applyMiddleware(sagaMiddleware),
+);
+
+sagaMiddleware.run(watchFetchQuotes);
 
 export default store;
